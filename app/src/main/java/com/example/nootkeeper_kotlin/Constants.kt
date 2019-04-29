2f3375a0d0db3b19eac9788f863996eb91388ed5
ZihuiKarl <karlyanggp@gmail.com>
2019-04-29 17:18:40 -0400

    Android Apps with Kotlin - 6

---
+++ app/src/main/java/com/example/nootkeeper_kotlin/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.nootkeeper_kotlin
+
+const val EXTRA_NOTE_POSITION = "EXTRA_NOTE_POSITION"
+const val POSITION_NOTE_SET = -1
--- app/src/main/java/com/example/nootkeeper_kotlin/DataManager.kt
+++ app/src/main/java/com/example/nootkeeper_kotlin/DataManager.kt
@@ -1,11 +1,12 @@
 package com.example.nootkeeper_kotlin
 
-class DataManager {
+object DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
 
     init {
         initializeCourses()
+        initializeNotes()
     }
 
     private fun initializeCourses(){
@@ -21,4 +22,43 @@ class DataManager {
         course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
         courses.set(course.courseId, course)
     }
+
+    private fun initializeNotes() {
+        var note = NoteInfo(courses.getValue("android_intents"),
+            "Dynamic intent resolution",
+            "Wow, intents allow componenets to be resovled at runtime")
+        notes.add(note)
+
+        note = NoteInfo(courses.getValue("android_intents"),
+            "Delegating intents",
+            "PendingIntents are powerful: they delegate much more than just a component invocation")
+        notes.add(note)
+
+        note = NoteInfo(courses.getValue("android_intents"),
+            "Service default threads",
+            "Did you know that by default an Android Service will tie up the UI thread?")
+        notes.add(note)
+
+        note = NoteInfo(courses.getValue("android_intents"),
+            "Long running operations",
+            "Foreground Services can be tied to a notification icon")
+        notes.add(note)
+
+        note = NoteInfo(courses.getValue("java_lang"),
+            "Parameters",
+            "Leverage variable-length parameter lists")
+        notes.add(note)
+
+        note = NoteInfo(courses.getValue("java_lang"),
+            "Anonymous classes",
+            "Anonymous classes simplify implmenting one-use type")
+        notes.add(note)
+
+        note = NoteInfo(courses.getValue("java_lang"),
+            "Compiler options",
+            "The -jar option isn't compatible with the -cp option")
+        notes.add(note)
+    }
+
+
 }
