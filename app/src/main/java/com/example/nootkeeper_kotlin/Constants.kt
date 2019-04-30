b8685004d0d967dd25fe208af83ba16787cb8d9b
ZihuiKarl <karlyanggp@gmail.com>
2019-04-30 16:55:16 -0400

    RecyclerView and Navigation Drawer - 5

--- app/src/main/java/com/example/nootkeeper_kotlin/Constants.kt
+++ app/src/main/java/com/example/nootkeeper_kotlin/Constants.kt
@@ -1,4 +1,4 @@
 package com.example.nootkeeper_kotlin
 
-const val EXTRA_NOTE_POSITION = "EXTRA_NOTE_POSITION"
+const val NOTE_POSITION = "NOTE_POSITION"
 const val POSITION_NOTE_SET = -1
--- app/src/main/java/com/example/nootkeeper_kotlin/NoteActivity.kt
+++ app/src/main/java/com/example/nootkeeper_kotlin/NoteActivity.kt
@@ -1,21 +1,20 @@
 package com.example.nootkeeper_kotlin
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
 
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.activity_note.*
+import kotlinx.android.synthetic.main.content_note.*
 
-class MainActivity : AppCompatActivity() {
+class NoteActivity : AppCompatActivity() {
     private var notePosition = POSITION_NOTE_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_note)
         setSupportActionBar(toolbar)
 
         val adapterCourses = ArrayAdapter<CourseInfo>(this,
@@ -25,11 +24,20 @@ class MainActivity : AppCompatActivity() {
 
         spinnerCourses.adapter = adapterCourses
 
-        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOTE_SET)
+        notePosition = savedInstanceState?.getInt(NOTE_POSITION, POSITION_NOTE_SET) ?:
+            intent.getIntExtra(NOTE_POSITION, POSITION_NOTE_SET)
 
         if(notePosition != POSITION_NOTE_SET){
             displayNote()
+        } else {
+            DataManager.notes.add(NoteInfo())
+            notePosition = DataManager.notes.lastIndex
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(NOTE_POSITION, notePosition)
     }
 
     private fun displayNote() {
@@ -53,7 +61,40 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
+            R.id.action_next -> {
+                moveNext()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun moveNext() {
+        ++notePosition
+        displayNote()
+        invalidateOptionsMenu()
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if(notePosition >= DataManager.notes.lastIndex){
+            val menuItem = menu?.findItem(R.id.action_next)
+            if(menuItem != null){
+                menuItem.icon = getDrawable(R.drawable.ic_block_white_24dp)
+                menuItem.isEnabled = false
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveNote()
+    }
+
+    private fun saveNote() {
+        val note = DataManager.notes[notePosition]
+        note.title = textNoteTitle.text.toString()
+        note.text = textNoteText.text.toString()
+        note.course = spinnerCourses.selectedItem as CourseInfo
     }
 }
